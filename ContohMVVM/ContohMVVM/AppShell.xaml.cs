96e7e318f17cc03432f10c9a8788ff42a05ffe42
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-08 13:32:06 +0700

    Add project files.

---
+++ ContohMVVM/ContohMVVM/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ContohMVVM.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ContohMVVM
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
