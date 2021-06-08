62dc0ec018364309483f9df27e7c9a1e00d95447
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-08 15:17:55 +0700

    menambahkan mvvm viewmodel

--- ContohMVVM/ContohMVVM/ViewModels/CoffeeViewModel.cs
+++ ContohMVVM/ContohMVVM/ViewModels/CoffeeViewModel.cs
@@ -1,39 +1,40 @@
-﻿using System;
+﻿using ContohMVVM.Models;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace ContohMVVM.ViewModels
 {
-    public class CoffeeViewModel : BindableObject
+    public class CoffeeViewModel : ViewModelBase
     {
-        public ICommand IncreaseCount { get; }
-        private string countDisplay = "Click Me";
+        public ObservableRangeCollection<Coffee> Coffee { get; set; }
+
+        public AsyncCommand RefreshCommand { get; }
 
         public CoffeeViewModel()
         {
-            IncreaseCount = new Command(OnIncrease);
+            Title = "Coffee Equipment";
+            Coffee = new ObservableRangeCollection<Coffee>();
+            var image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min.png";
+            Coffee.Add(new Coffee { Roaster="Dark Roast",Name="Italian Roast", Image=image });
+            Coffee.Add(new Coffee { Roaster="Medium Roast",Name="Colombia",Image=image});
+            Coffee.Add(new Coffee { Roaster = "Medium Roast", Name = "Pike Place", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Medium Roast", Name = "Brazilian", Image = image });
+
+            RefreshCommand = new AsyncCommand(Refresh);
         }
 
-        public string CountDisplay
+        private async Task Refresh()
         {
-            get { return countDisplay; }
-            set
-            {
-                if (value == countDisplay)
-                    return;
-
-                countDisplay = value;
-                OnPropertyChanged();
-            }
-        }
-
-        int count = 0;
-        private void OnIncrease()
-        {
-            count++;
-            CountDisplay = $"Anda klik {count} time";
+            IsBusy = true;
+            await Task.Delay(2000);
+            IsBusy = false;
         }
     }
 }
