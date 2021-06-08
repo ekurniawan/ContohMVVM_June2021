f08bcff2e7328fccaf07df311e6f9bafb83933c0
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-08 14:00:25 +0700

    menambahkan simple binding

--- ContohMVVM/ContohMVVM/Views/CoffeePage.xaml.cs
+++ ContohMVVM/ContohMVVM/Views/CoffeePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,39 @@ namespace ContohMVVM.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CoffeePage : ContentPage
     {
+        public ICommand IncreaseCount { get; }
+
         public CoffeePage()
         {
             InitializeComponent();
+            IncreaseCount = new Command(OnIncrease);
+            
+            BindingContext = this;
+
         }
+
+        int count = 0;
+        private void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"Anda klik {count} time";
+        }
+
+        private string countDisplay="Click Me";
+        public string CountDisplay
+        {
+            get { return countDisplay; }
+            set {
+                if (value == countDisplay)
+                    return;
+
+                countDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        
+       
     }
 }
