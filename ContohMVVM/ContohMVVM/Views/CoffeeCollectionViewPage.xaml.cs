e09b165f20f36653feacc2ae81bf6e23e4d91901
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-09 13:09:28 +0700

    menambahkan collectionview

---
+++ ContohMVVM/ContohMVVM/Views/CoffeeCollectionViewPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ContohMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContohMVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoffeeCollectionViewPage : ContentPage
+    {
+        public CoffeeCollectionViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var data = e.CurrentSelection[0] as Coffee;
+            await DisplayAlert("Favorite Coffee", data.Name, "OK");
+        }
+    }
+}
