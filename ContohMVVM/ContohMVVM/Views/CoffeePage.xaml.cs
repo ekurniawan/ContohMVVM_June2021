ca766baf81155578718de50f237f2c56135d17dc
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-09 09:33:58 +0700

    menambahkan context action

--- ContohMVVM/ContohMVVM/Views/CoffeePage.xaml.cs
+++ ContohMVVM/ContohMVVM/Views/CoffeePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ContohMVVM.ViewModels;
+﻿using ContohMVVM.Models;
+using ContohMVVM.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,26 @@ namespace ContohMVVM.Views
             InitializeComponent();
             //BindingContext = new CoffeeViewModel();
         }
-       
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var coffee = ((ListView)sender).SelectedItem as Coffee;
+            if (coffee == null)
+                return;
+            await DisplayAlert("Coffee Selected", coffee.Name, "OK");
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var coffee = ((MenuItem)sender).BindingContext as Coffee;
+            if (coffee == null)
+                return;
+            await DisplayAlert("Coffee Favorite", coffee.Name, "OK");
+        }
     }
 }
