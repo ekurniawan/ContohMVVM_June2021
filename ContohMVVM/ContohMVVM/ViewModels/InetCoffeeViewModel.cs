0737e64a74e044fb5d958ea4d9ef38dadaaffbe7
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-10 16:29:46 +0700

    menambahkan

--- ContohMVVM/ContohMVVM/ViewModels/InetCoffeeViewModel.cs
+++ ContohMVVM/ContohMVVM/ViewModels/InetCoffeeViewModel.cs
@@ -1,5 +1,5 @@
-﻿using ContohMVVM.Shared.Models;
-using ContohMVVM.Services;
+﻿using ContohMVVM.Services;
+using ContohMVVM.Shared.Models;
 using ContohMVVM.Views;
 using MvvmHelpers;
 using MvvmHelpers.Commands;
@@ -7,21 +7,22 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ContohMVVM.ViewModels
 {
-    public class CoffeeSQLiteViewModel : ViewModelBase
+    public class InetCoffeeViewModel : ViewModelBase
     {
         public ObservableRangeCollection<Coffee> Coffee { get; set; }
-        public AsyncCommand RefreshCommand { get;  }
-        public AsyncCommand AddCommand { get;  }
+        public AsyncCommand RefreshCommand { get; }
+        public AsyncCommand AddCommand { get; }
         public AsyncCommand<Coffee> RemoveCommand { get; }
         public AsyncCommand<object> SelectedCommand { get; }
 
-        
+        private ICoffee coffeeServices;
 
-        public CoffeeSQLiteViewModel()
+        public InetCoffeeViewModel()
         {
             Title = "My Coffee SQLite";
             Coffee = new ObservableRangeCollection<Coffee>();
@@ -30,6 +31,8 @@ namespace ContohMVVM.ViewModels
             AddCommand = new AsyncCommand(Add);
             RemoveCommand = new AsyncCommand<Coffee>(Remove);
             SelectedCommand = new AsyncCommand<object>(Selected);
+
+            coffeeServices = new InetCoffeeService();
         }
 
         private async Task Selected(object arg)
@@ -43,27 +46,35 @@ namespace ContohMVVM.ViewModels
 
         private async Task Remove(Coffee coffee)
         {
-            await CoffeeService.RemoveCoffee(coffee.Id);
+            await coffeeServices.RemoveCoffee(coffee.Id);
             await Refresh();
             //await App.Current.MainPage.DisplayAlert("Keterangan", coffee.Name, "OK");
         }
 
         private async Task Add()
         {
-            //var name = await App.Current.MainPage.DisplayPromptAsync("Name", "Coffee Name");
-            //var roaster = await App.Current.MainPage.DisplayPromptAsync("Roaster", "Coffee Roaster");
-            //await CoffeeService.AddCoffee(name, roaster);
-            var route = $"{nameof(AddCoffeeSQLitePage)}";
-            await Shell.Current.GoToAsync(route);
+            var name = await App.Current.MainPage.DisplayPromptAsync("Name", "Coffee Name");
+            var roaster = await App.Current.MainPage.DisplayPromptAsync("Roaster", "Coffee Roaster");
+            await coffeeServices.AddCoffee(name, roaster);
+            await Refresh();
+            //var route = $"{nameof(AddCoffeeSQLitePage)}";
+            //await Shell.Current.GoToAsync(route);
         }
 
         private async Task Refresh()
         {
+            var current = Connectivity.NetworkAccess;
+            if (current != NetworkAccess.Internet)
+            {
+                await Application.Current.MainPage.DisplayAlert("Keterangan",
+                    "Tidak ada koneksi internet !", "OK");
+            }
+
             IsBusy = true;
             //await Task.Delay(1000);
 
             Coffee.Clear();
-            var results = await CoffeeService.GetCoffee();
+            var results = await coffeeServices.GetCoffee();
             Coffee.AddRange(results);
 
 
