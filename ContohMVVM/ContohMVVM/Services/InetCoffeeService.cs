badf58c1ee15d34fe9d7907fd381c2795591773a
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-10 14:24:57 +0700

    menambahkan add data

--- ContohMVVM/ContohMVVM/Services/InetCoffeeService.cs
+++ ContohMVVM/ContohMVVM/Services/InetCoffeeService.cs
@@ -1,4 +1,5 @@
 ﻿using ContohMVVM.Shared.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -9,20 +10,39 @@ namespace ContohMVVM.Services
 {
     public class InetCoffeeService : ICoffee
     {
-        string baseUrl = "https://coffeebackendservices.azurewebsites.net/";
+        string baseUrl = "https://coffeebackendservices.azurewebsites.net";
         private HttpClient _client;
 
         public InetCoffeeService()
         {
-            _client = new HttpClient
-            {
-                BaseAddress = new Uri(baseUrl)
-            };
+            _client = new HttpClient();
         }
 
-        public Task AddCoffee(string name, string roaster)
+        public async Task AddCoffee(string name, string roaster)
         {
-            throw new NotImplementedException();
+            Random rnd = new Random();
+            var uri = new Uri($"{baseUrl}/api/Coffee");
+            try
+            {
+                var newCoffee = new Coffee
+                {
+                    Id = rnd.Next(0,10000),
+                    Name = name,
+                    Roaster = roaster,
+                    Image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min.png"
+                };
+                var json = JsonConvert.SerializeObject(newCoffee);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _client.PostAsync(uri, content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Gagal menambahkan data");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error : {ex.Message}");
+            }
         }
 
         public Task EditCoffee(int Id, Coffee coffee)
@@ -30,9 +50,24 @@ namespace ContohMVVM.Services
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Coffee>> GetCoffee()
+        public async Task<IEnumerable<Coffee>> GetCoffee()
         {
-            throw new NotImplementedException();
+            List<Coffee> lstCoffee = new List<Coffee>();
+            var uri = new Uri($"{baseUrl}/api/Coffee");
+            try
+            {
+                var response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    lstCoffee = JsonConvert.DeserializeObject<List<Coffee>>(content);
+                }
+                return lstCoffee;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
         }
 
         public Task<Coffee> GetCoffee(int id)
